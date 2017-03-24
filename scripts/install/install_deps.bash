18d41a811fc7f271ab60037a23b0d7d8771a9061
Chris Choi <chutsu@gmail.com>
2017-03-24 11:47:07 -0400

    Add boost as part of the travis build

--- scripts/install/install_deps.bash
+++ scripts/install/install_deps.bash
@@ -19,6 +19,7 @@ install_dependency() {
     fi
 }
 
+install_dependency "Boost" $SCRIPT_PATH/boost_install.bash
 install_dependency "Ceres" $SCRIPT_PATH/ceres_install.bash
 install_dependency "Eigen" $SCRIPT_PATH/eigen_install.bash
 install_dependency "Yaml-CPP" $SCRIPT_PATH/yaml_cpp_install.bash
