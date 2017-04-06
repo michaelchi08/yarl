87bb3fa405af1384ff56e3f7ee3e83b517a6d471
Chris Choi <chutsu@gmail.com>
2017-04-06 14:30:08 -0400

    Add ci_run.bash

---
+++ scripts/ci_run.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e  # exit on first error
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+CMAKE_CONFIG_DIR="../cmake"  # cmake folder relative to this script
+
+
+# submodules
+git submodule init
+git submodule update
+
+# compile
+mkdir -p build
+cd build
+cmake .. -DCMAKE_MODULE_PATH=$DIR/$CMAKE_CONFIG_DIR
+make
+
+# run tests
+export LD_LIBRARY_PATH=/usr/local/lib
+./tests/yarl_tests --silence-stdcout
